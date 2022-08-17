5d209f70f699f1215b2ae57f1f2b9b147b8e7300
Aniket Jagtap <aniket.jagtap206@gmail.com>
2022-08-17 23:42:35 +0530

    comments added

--- Arrays/RemDup.java
+++ Arrays/RemDup.java
@@ -1,13 +1,16 @@
+// leetcode solution : To return remove the duplicate values from the sorted array and return 
+//the size of the array(with non duplicate values).
+
 public class RemDup {
     public static void main(String[] args) {
-        int[] arr = { 0, 1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 5 };
+        int[] arr = { 0, 1, 1, 2, 2, 2, 3, 3, 4 };
         System.out.println(removeDuplicates(arr));
     }
 
     static int removeDuplicates(int[] arr) {
         int count = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (i < arr.length && arr[i] == arr[i + 1]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1 && arr[i] == arr[i + 1]) {
                 continue;
             }
             arr[count] = arr[i];
